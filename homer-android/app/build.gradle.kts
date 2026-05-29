5f2e9c7c5f312bf83d8c502c3cd40444ac9d7e3c
Bogdan Radu <bogdan.radu@b4it.ro>
2026-05-29 07:37:58 +0300

    chore: versionCode 25 + versionName 2.1.1-20260529

--- homer-android/app/build.gradle.kts
+++ homer-android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.homer.com"
         minSdk = 26
         targetSdk = 35
-        versionCode = 24
-        versionName = "2.1.0-20260528"
+        versionCode = 25
+        versionName = "2.1.1-20260529"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
