4b837b4915ffc7ace023f469ac625cdd2762a5a3
Bogdan Radu <bogdan.radu@b4it.ro>
2026-07-30 13:54:28 +0300

    fix: unify displayed version as 2.2.1

--- homer-android/app/build.gradle.kts
+++ homer-android/app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.homer.com"
         minSdk = 26
         targetSdk = 35
-        versionCode = 34
-        versionName = "2.2.0-20260730"
+        versionCode = 35
+        versionName = "2.2.1-20260730"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
